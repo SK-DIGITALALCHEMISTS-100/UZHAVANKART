a0057c08ab103ea4493214daa23606531202ac75
Balaji S <balajisara100@gmail.com>
2026-03-11 09:28:50 +0530

    fix: clean forms.py and fix cloudinary settings

--- uzhavankart/views.py
+++ uzhavankart/views.py
@@ -1,3 +1,10 @@
+import os
+import cloudinary
+cloudinary.config(
+    cloud_name=os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    api_key=os.environ.get('CLOUDINARY_API_KEY'),
+    api_secret=os.environ.get('CLOUDINARY_API_SECRET'),
+)
 from django.shortcuts import render,get_object_or_404, redirect
 
 # Create your views here.
