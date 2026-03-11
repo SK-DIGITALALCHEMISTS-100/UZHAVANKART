089a5184e09ff538c30d5a2869e842878af4e64b
Balaji S <balajisara100@gmail.com>
2026-03-11 10:41:22 +0530

    hi1

--- uk/settings.py
+++ uk/settings.py
@@ -69,15 +69,15 @@ else:
             'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
-
-# Email
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'digitalalchemists00@gmail.com')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+EMAIL_USE_SSL = False
+EMAIL_TIMEOUT = 10
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
