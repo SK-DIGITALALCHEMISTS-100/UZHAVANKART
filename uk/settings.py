1d782ff807a91132de76d9530337e9ab3d1a15f6
Balaji S <balajisara100@gmail.com>
2026-04-10 18:24:18 +0530

    new file add

--- uk/settings.py
+++ uk/settings.py
@@ -94,7 +94,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
+# WhiteNoise Configuration for Vercel
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+WHITENOISE_MANIFEST_STRICT = False
+WHITENOISE_USE_FINDERS = True
 
 # Cloudinary Media Storage
 CLOUDINARY_STORAGE = {
