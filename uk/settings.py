40fadbeca52e04898cbc827d72b37f483b768fe8
Balaji S <balajisara100@gmail.com>
2026-04-10 23:21:36 +0530

    setting file update

--- uk/settings.py
+++ uk/settings.py
@@ -7,10 +7,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Security
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-1^as155o(j45mc=7a6si06(464&+))%getrc=40l!(8n72ano=')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '.railway.app,localhost').split(',')
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://uzhavankart-production.up.railway.app"
+    "https://uzhavankart-production.up.railway.app",
+    "https://*.railway.app"
 ]
 
 INSTALLED_APPS = [
