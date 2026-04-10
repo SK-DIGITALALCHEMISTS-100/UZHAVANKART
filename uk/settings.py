02fc80197b9108b0d2ec86d9c82686bb86f86524
Balaji S <balajisara100@gmail.com>
2026-04-10 23:28:34 +0530

    setting giles

--- uk/settings.py
+++ uk/settings.py
@@ -106,5 +106,6 @@ CLOUDINARY_STORAGE = {
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
