9d3ef1eab79af2b0bf9548009fe7adb6f04de232
Balaji S <balajisara100@gmail.com>
2026-05-11 22:26:20 +0530

    urls update

--- uk/urls.py
+++ uk/urls.py
@@ -1,37 +1,23 @@
 """
-URL configuration for uk project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+Main URL Configuration for UZHAVANKART Project
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.http import HttpResponse
 from django.conf import settings
 from django.conf.urls.static import static
 
-def home(request):
-    return HttpResponse("UzhavanKart is Live 🚀")
-
-def health_check(request):
-    return HttpResponse("OK")
-
 urlpatterns = [
-    path('', home),
-    path('health/', health_check),
+    # Admin panel
     path('admin/', admin.site.urls),
-    path('uzhavankart/', include('uzhavankart.urls')),
+    
+    # Health check for deployment
+    path('health/', lambda request: __import__('django.http', fromlist=['HttpResponse']).HttpResponse('OK')),
+    
+    # Include all app URLs from uzhavankart app (no prefix - routes already have paths)
+    path('', include('uzhavankart.urls')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
+# Serve media files in development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
